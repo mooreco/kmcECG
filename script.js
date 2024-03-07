713313383090d7a9e0bd958406efef5746e4b8c6
mooreco <mooreco@byui.edu>
2024-03-07 10:46:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,21 @@ document.addEventListener("DOMContentLoaded", function() {
   });
   
   function generateEmbedCode() {
-    var entryId = document.getElementById("entryIdInput").value;
-    var playerId = "kaltura_player_1687278321"; // You can customize the player ID here
+  var entryId = document.getElementById("entryIdInput").value;
+  var videoType = document.querySelector('input[name="videoType"]:checked').value;
   
-    var embedCode = '<iframe id="' + playerId + '" src="https://cdnapisec.kaltura.com/p/1157612/sp/115761200/embedIframeJs/uiconf_id/47306393/partner_id/1157612?iframeembed=true&playerId=' + playerId + '&entry_id=' + entryId + '" width="480" height="270" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" frameborder="0"></iframe>';
+  var playerId = "kaltura_player_" + (new Date()).getTime(); // Unique player ID for each generation
   
-    var link = 'https://cdnapisec.kaltura.com/p/1157612/sp/115761200/embedIframeJs/uiconf_id/47306393/partner_id/1157612?iframeembed=true&playerId=' + playerId + '&entry_id=' + entryId;
+  var uiconf_id = videoType === "downloadable" ? "47306393" : "42438262"; // Adjusted based on video type
   
-    document.getElementById("embedCodeOutput").value = embedCode;
-    document.getElementById("linkOutput").value = link;
-  }
+  var embedCode = '<iframe id="' + playerId + '" src="https://cdnapisec.kaltura.com/p/1157612/sp/115761200/embedIframeJs/uiconf_id/' + uiconf_id + '/partner_id/1157612?iframeembed=true&playerId=' + playerId + '&entry_id=' + entryId + '" width="480" height="270" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" frameborder="0"></iframe>';
+
+  var link = 'https://cdnapisec.kaltura.com/p/1157612/sp/115761200/embedIframeJs/uiconf_id/' + uiconf_id + '/partner_id/1157612?iframeembed=true&playerId=' + playerId + '&entry_id=' + entryId;
+
+  document.getElementById("embedCodeOutput").value = embedCode;
+  document.getElementById("linkOutput").value = link;
+}
+
   
   function copyEmbedCode() {
     var embedCodeOutput = document.getElementById("embedCodeOutput");
